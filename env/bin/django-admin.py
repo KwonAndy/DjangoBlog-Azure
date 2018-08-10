b4b71ef3a8d01876bad6f34208aecc75b8361dc6
Andy <kwona714@hotmail.com>
2018-08-10 00:30:14 -0700

    initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Andy/uwPython230/django-blog-2-master/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myblog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from myblog.models import Post
+from myblog.models import Category
+
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ['posts',]
